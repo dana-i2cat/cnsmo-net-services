e7228389c54f3e83a8f2dc00181182d138d3002d
Moya, Oscar <oscar.moya@i2cat.net>
2016-03-17 12:34:51 +0100

    Remove -t flag of the EasyRSA Docker
    Since the scripts are meant to be called by a remote python process,
    some times there are problems related with tty options. To avoid that
    the esiest way is to remove that flag  and not use the shell=True
    on python subprocess calls

--- src/main/docker/vpn/easy-rsa/gen_ca.sh
+++ src/main/docker/vpn/easy-rsa/gen_ca.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
+echo -e "Generating Index and serial"
 sh "$( cd "$( dirname "$0" )" && pwd )"/gen_index.sh
-
-echo -e "Generating CA certificate and key. Note: Default variables can be edit in vars.example\n"
-
-docker run -it --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-ca
-
-echo -e "\nGenerating server certificate and keys.\n"
-
-#docker run -it --rm --name easy-rsa -v $(pwd)/keys:/easy-rsa --env-file vars carbonsphere/dock-easy-rsa /er/build-key-server $srvname
-
-docker run -it --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-dh
-
-echo -e "Moving necessary files to openvpn directory\n"
-
-#cp ./keys/ca.crt ./openvpn
-#cp ./keys/$srvname.crt ./openvpn
-#cp ./keys/$srvname.key ./openvpn
-#p ./keys/dh2048.pem ./openvpn
-
-#ls -l ./openvpn
+echo -e "Generating CA certificate and key.\n"
+docker run -i --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-ca
+docker run -i --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-dh
--- src/main/docker/vpn/easy-rsa/gen_client.sh
+++ src/main/docker/vpn/easy-rsa/gen_client.sh
@@ -1,8 +1,7 @@
-
 echo -e "\nGenerating client certificate and keys.\n"
 srvname="client"
 echo -e "\nCreating $srvname"
 
-docker run -it --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-key $srvname
+docker run -i --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-key $srvname
 
 
