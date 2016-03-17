dd59ace7c5503832cb7e1c9bcd292e24a6d53529
Isart Canyameres <isart.canyameres@i2cat.net>
2016-03-17 10:34:18 +0100

    Install docker in any linux supported by docker.

--- src/main/docker/vpn/server/install_docker.sh
+++ src/main/docker/vpn/server/install_docker.sh
@@ -1,46 +1,21 @@
 #!/bin/bash
 
-#####################################
-## Install docker on ubuntu trusty ##
-#####################################
+#############################
+## Install docker on linux ##
+#############################
 
 # following the official guide here (accessed on 14/03/2016):
-# https://docs.docker.com/engine/installation/linux/ubuntulinux/
+# https://docs.docker.com/linux/step_one/
 
-echo "updating docker apt sources ..."
-
-# ensure that APT works with the https method, and that CA certificates are installed.
-apt-get update
-apt-get install apt-transport-https ca-certificates
-
-# Add docker PGP key
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-
-# Add docker packages repo for ubuntu-trusty
-touch /etc/apt/sources.list.d/docker.list
-echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-apt-get update
-
-# Uninstall old repo if it exists (it does not in the clean ubuntu/trusty64)
-apt-get purge lxc-docker
-
-
-echo "installing docker prerequisites ..."
-
-# For Ubuntu Trusty, it’s recommended to install the linux-image-extra kernel package.
-# The linux-image-extra package allows you use the aufs storage driver.
-apt-get install linux-image-extra-$(uname -r) -y
-
-# If you are installing on Ubuntu 14.04, apparmor is required (already installed in ubuntu/trusty64)
-apt-get install apparmor
+# Having curl installed is a requirement.
+# Must use OS dependent instructions to install it, before calling this script.
 
 echo "installing docker ..."
 
-# Install Docker
-apt-get update
-apt-get install docker-engine -y
+# Add Docker PGP key
+curl -fsSL https://get.docker.com/gpg | apt-key add -
 
-# Start docker daemon
-service docker start
+# Run Docker installation script
+curl -fsSL https://get.docker.com/ | sh
 
 echo "done installing docker."
