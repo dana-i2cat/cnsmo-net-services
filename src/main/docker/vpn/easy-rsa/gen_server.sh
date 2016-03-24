15d8769671624b437df7500514dc3e7b51ebc8cb
Moya, Oscar <oscar.moya@i2cat.net>
2016-03-24 16:48:26 +0100

    Refactor Key Server generation

--- src/main/docker/vpn/easy-rsa/gen_server.sh
+++ src/main/docker/vpn/easy-rsa/gen_server.sh
@@ -1,5 +1,5 @@
 srvname="server"
 echo -e "\nCreating $srvname"
-docker run -i --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-key $srvname
+docker run -i --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-key-server $srvname
 
 
