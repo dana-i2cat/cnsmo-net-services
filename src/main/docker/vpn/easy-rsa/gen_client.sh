0a17bb5475be320ff9599844522c455af1191e54
Isart Canyameres <isart.canyameres@i2cat.net>
2016-04-06 14:41:09 +0200

    Allow specifying name for client certs.

--- src/main/docker/vpn/easy-rsa/gen_client.sh
+++ src/main/docker/vpn/easy-rsa/gen_client.sh
@@ -1,5 +1,14 @@
 echo -e "\nGenerating client certificate and keys.\n"
+
 srvname="client"
+while getopts u: option
+do
+        case "${option}"
+        in
+                u) srvname=${OPTARG};;
+        esac
+done
+
 echo -e "\nCreating $srvname"
 
 docker run -i --rm --name easy-rsa -v "$( cd "$( dirname "$0" )" && pwd )"/keys:/easy-rsa --env-file "$( cd "$( dirname "$0" )" && pwd )"/vars new-easy-rsa /er/build-key $srvname
